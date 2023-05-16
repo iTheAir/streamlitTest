107231ed14a242493b0c3d3127d3aea3e500cf0a
iTheAir <76616657+iTheAir@users.noreply.github.com>
2023-05-16 11:33:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import barfi as bi
-
+from barfi import st_barfi, Block
 
 st.title("This is the App title")
 x = st.slider('Select a value')
@@ -10,3 +10,13 @@ st.checkbox('yes')
 st.sidebar.title('This is written inside the sidebar')
 st.sidebar.button('click')
 st.sidebar.radio('Pick your gender', ["male", "female"])
+
+
+
+feed = Block(name='Feed')
+feed.add_output()
+
+result = Block(name='Result')
+result.add_output()
+
+st_barfi(base_blocks=[feed, result])
