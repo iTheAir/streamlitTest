98505ef5809cdd9ebd95731998854492479b9648
iTheAir <76616657+iTheAir@users.noreply.github.com>
2023-05-16 11:15:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
+st.title("This is the App title")
 x = st.slider('Select a value')
 st.write(x, 'squred is', x * x)
+st.header("If you like, check below")
+st.checkBox('yes')
